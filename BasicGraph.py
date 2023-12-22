e5bb382689224dbb52f794b905ee2e0841006a41
Timothy Macfarlane <thecyberlocal@gmail.com>
2023-12-22 11:06:01 -0600

    Update and rename BasicGraph.py to simple-graph.py
    Added verbose inline commentary.

--- BasicGraph.py
+++
@@ -1,4 +0,0 @@
-from matplotlib import pyplot as plt
-
-plt.plot([5, 6, 7, 8], [7, 3, 8, 3])
-plt.show()
---
+++ simple-graph.py
@@ -0,0 +1,4 @@
+from matplotlib import pyplot  # Import the graphing library
+
+pyplot.plot([5, 6, 7, 8], [7, 3, 8, 3])  # Passing in x value array and corresponding y value array
+pyplot.show()  # Display the graph with automatic sizing
