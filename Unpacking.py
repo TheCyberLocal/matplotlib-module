d40650e4c0cb206011fc78626ee01ef8a9efe1f9
Timothy Macfarlane <thecyberlocal@gmail.com>
2023-12-22 11:32:54 -0600

    Update Unpacking.py
    Added verbose inline commentary with explanatory header.

--- Unpacking.py
+++ Unpacking.py
@@ -1,11 +1,18 @@
-from matplotlib import pyplot as plt
-import numpy as np
+"""
+The script is particularly useful for scenarios where data is stored externally and needs to be visualized.
+It simplifies the process of data loading and plotting, making it ideal for quick analysis and presentations.
+"""
 
+from matplotlib import pyplot as plt  # Import pyplot for creating plots.
+import numpy as np  # Import numpy for numerical operations.
 
-x, y = np.loadtxt(r'filepath',
-                  unpack=True,
-                  delimiter=',')
+# Load data from a file.
+x, y = np.loadtxt(r'filepath',  # Replace 'filepath' with the actual file path.
+                  unpack=True,  # Unpacks the columns into separate arrays.
+                  delimiter=',')  # Specifies the delimiter in the file.
 
-plt.plot(x, y, label='loaded from file')
+# Create a plot with the loaded data.
+plt.plot(x, y, label='loaded from file')  # Plot the data with a label.
 
-plt.show()
+# Display the plot.
+plt.show()  # Render and show the plot.
